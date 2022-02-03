509037d24d3690ec10dbc80e2882c10c51c7a964
JNKirshner <98779332+JNKirshner@users.noreply.github.com>
2022-02-03 09:25:24 -0500

    Date Range feature

--- ui.R
+++ ui.R
@@ -11,6 +11,11 @@ ui <- fluidPage(
               choices= unique(sales_df$pcode
               )
   ),
+  dateRangeInput(inputId =  'selected_date_range',
+                 label= "Select a Date range",
+                 min = min(sales_df$date),
+                 max = max(sales_df$date),
+                 ),
   plotOutput("ts_plot"),
   verbatimTextOutput('debug')
 )
